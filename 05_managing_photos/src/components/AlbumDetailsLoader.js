bc9839fa27f0c0a32a0895bebdfe00a8e24ed1fa
Lana-Banana <bran0521@gmail.com>
2019-05-01 16:33:26 +0900

    05_managing_photos

---
+++ 05_managing_photos/src/components/AlbumDetailsLoader.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+// import { Connect } from 'aws-amplify-react';
+import { API, graphqlOperation } from 'aws-amplify';
+import { GetAlbum } from './graphql';
+import AlbumDetails from './AlbumDetails';
+
+export default class AlbumDetailsLoader extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            nextTokenForPhotos: null,
+            hasMorePhotos: true,
+            album: null,
+            loading: true
+        };
+    }
+
+    async loadMorePhotos() {
+        if (!this.state.hasMorePhotos) return;
+
+        this.setState({ loading: true });
+        const { data } = await API.graphql(graphqlOperation(GetAlbum, {id: this.props.id, nextTokenForPhotos: this.state.nextTokenForPhotos}));
+
+        let album;
+        if (this.state.album === null) {
+            album = data.getAlbum;
+        } else {
+            album = this.state.album;
+            album.photos.items = album.photos.items.concat(data.getAlbum.photos.items);
+        }
+        this.setState({ 
+            album: album,
+            loading: false,
+            nextTokenForPhotos: data.getAlbum.photos.nextToken,
+            hasMorePhotos: data.getAlbum.photos.nextToken !== null
+        });
+    }
+
+    componentDidMount() {
+        this.loadMorePhotos();
+    }
+
+    render() {
+        return (
+            <AlbumDetails 
+                loadingPhotos={this.state.loading} 
+                album={this.state.album} 
+                loadMorePhotos={this.loadMorePhotos.bind(this)} 
+                hasMorePhotos={this.state.hasMorePhotos} 
+            />
+        );
+    }
+}
+
