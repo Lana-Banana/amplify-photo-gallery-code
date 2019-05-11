267dd41dfe78827d827227b5a5f76f0523625026
Lana-Banana <bran0521@gmail.com>
2019-05-11 09:41:20 +0900

    add template

---
+++ photo-albums/src/App.js
@@ -0,0 +1,16 @@
+// src/App.js
+
+import React, { Component } from 'react';
+import { Header } from 'semantic-ui-react';
+
+class App extends Component { 
+    render() { 
+        return (
+            <div>
+                <Header as='h1'>Hello World!</Header>
+            </div>
+        );
+    }
+}
+
+export default App;
